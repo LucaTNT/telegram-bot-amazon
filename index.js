86d4cb7f7376c4f24b7c0e7b6ba784459c15fa6b
amit05012 <67558555+amit05012@users.noreply.github.com>
2020-12-10 10:06:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /*
 telegram-bot-amazon
-
 Author: Luca Zorzi (@LucaTNT)
 License: MIT
 */
@@ -31,25 +30,11 @@ if (shorten_links && !bitly_token) {
   process.exit(1);
 }
 
-const raw_links = process.env.RAW_LINKS && process.env.RAW_LINKS == "true";
-
-var group_replacement_message;
-
-if (!process.env.GROUP_REPLACEMENT_MESSAGE) {
-  console.log(
-    "Missing GROUP_REPLACEMENT_MESSAGE env variable, using the default one"
-  );
-  group_replacement_message =
-    "Message by {USER} with Amazon affiliate link:\n\n{MESSAGE}";
-} else {
-  group_replacement_message = process.env.GROUP_REPLACEMENT_MESSAGE;
-}
-
 var amazon_tld;
 
 if (!process.env.AMAZON_TLD) {
-  console.log("Missing AMAZON_TLD env variable, using the default one (.com)");
-  amazon_tld = "com";
+  console.log("Missing AMAZON_TLD env variable, using the default one (.in)");
+  amazon_tld = "in";
 } else {
   amazon_tld = process.env.AMAZON_TLD;
 }
@@ -57,29 +42,10 @@ if (!process.env.AMAZON_TLD) {
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const amazon_tag = process.env.AMAZON_TAG;
 const rawUrlRegex = new RegExp(
-  `https?://(([^\\s]*)\\.)?amazon\\.${amazon_tld}/?([^\\s]*)`,
+  https?://(([^\\s]*)\\.)?amazon\\.${amazon_tld}/?([^\\s]*),
   "ig"
 );
 
-var usernames_to_ignore = [];
-var user_ids_to_ignore = [];
-
-if (process.env.IGNORE_USERS) {
-  const usernameRegex = /@([^\s]+)/gi;
-  const userIdRegex = /([0-9]+)/gi;
-  let to_ignore = process.env.IGNORE_USERS.split(",");
-  to_ignore.forEach((ignore) => {
-    let usernameResult = usernameRegex.exec(ignore.trim());
-    if (usernameResult) {
-      usernames_to_ignore.push(usernameResult[1].toLowerCase());
-    } else {
-      let userIdResult = userIdRegex.exec(ignore.trim());
-      if (userIdResult) {
-        user_ids_to_ignore.push(parseInt(userIdResult[1]));
-      }
-    }
-  });
-}
 
 const bot = new TelegramBot(token, { polling: true });
 
@@ -90,7 +56,7 @@ function log(msg) {
 
 async function shortenURL(url) {
   const headers = {
-    Authorization: `Bearer ${bitly_token}`,
+    Authorization: Bearer ${bitly_token},
     "Content-Type": "application/json",
   };
   const body = { long_url: url, domain: "bit.ly" };
@@ -108,13 +74,13 @@ async function shortenURL(url) {
       return url;
     }
   } catch (err) {
-    log(`Error in bitly response ${err}`);
+    log(Error in bitly response ${err});
     return url;
   }
 }
 
 function buildAmazonUrl(asin) {
-  return `https://www.amazon.${amazon_tld}/dp/${asin}?tag=${amazon_tag}`;
+  return https://www.amazon.${amazon_tld}/dp/${asin}?tag=${amazon_tag};
 }
 
 function buildRawAmazonUrl(element) {
@@ -131,174 +97,8 @@ async function getAmazonURL(element) {
       ? buildAmazonUrl(element.asin)
       : buildRawAmazonUrl(element);
   return shorten_links ? await shortenURL(url) : url;
-}
-
-function buildMention(user) {
-  return user.username
-    ? "@" + user.username
-    : user.first_name + (user.last_name ? " " + user.last_name : "");
-}
-
-async function buildMessage(chat, message, replacements, user) {
-  if (isGroup(chat)) {
-    var affiliate_message = message;
-    for await (const element of replacements) {
-      const sponsored_url = await getAmazonURL(element);
-      affiliate_message = affiliate_message.replace(
-        element.fullURL,
-        sponsored_url
-      );
-    }
-
-    return group_replacement_message
-      .replace(/\\n/g, "\n")
-      .replace("{USER}", buildMention(user))
-      .replace("{MESSAGE}", affiliate_message)
-      .replace("{ORIGINAL_MESSAGE}", message);
-  } else {
-    var text = "";
-    if (replacements.length > 1) {
-      for await (const element of replacements) {
-        text += "• " + (await getAmazonURL(element)) + "\n";
-      }
-    } else {
-      text = await getAmazonURL(replacements[0]);
-    }
-
-    return text;
-  }
-}
-
-function isGroup(chat) {
-  return chat.type == "group" || chat.type == "supergroup";
-}
-
-function deleteAndSend(chat, messageId, text) {
-  const chatId = chat.id;
-  var deleted = false;
-
-  if (isGroup(chat)) {
-    bot.deleteMessage(chatId, messageId);
-    deleted = true;
-  }
-  bot.sendMessage(chatId, text);
-
-  return deleted;
-}
-
-function getASINFromFullUrl(url) {
-  const match = fullURLRegex.exec(url);
-
-  return match != null ? match[8] : url;
-}
-
-async function getLongUrl(shortURL) {
-  try {
-    let res = await fetch(shortURL, { redirect: "manual" });
-    return { fullURL: res.headers.get("location"), shortURL: shortURL };
-  } catch (err) {
-    log("Short URL " + shortURL + " -> ERROR");
-    return null;
-  }
-}
-
-bot.on("message", async (msg) => {
-  try {
-    let from_username = msg.from.username
-      ? msg.from.username.toLowerCase()
-      : "";
-    let from_id = msg.from.id;
-    if (
-      (!usernames_to_ignore.includes(from_username) &&
-        !user_ids_to_ignore.includes(from_id)) ||
-      !isGroup(msg.chat)
-    ) {
-      shortURLRegex.lastIndex = 0;
-      var replacements = [];
-      var match;
-      if (raw_links) {
-        rawUrlRegex.lastIndex = 0;
-
-        while ((match = rawUrlRegex.exec(msg.text)) !== null) {
-          const fullURL = match[0];
-
-          replacements.push({ asin: null, fullURL: fullURL });
-        }
-      } else {
-        fullURLRegex.lastIndex = 0;
-
-        while ((match = fullURLRegex.exec(msg.text)) !== null) {
-          const asin = match[8];
-          const fullURL = match[0];
-          replacements.push({ asin: asin, fullURL: fullURL });
-        }
-      }
-
-      while ((match = shortURLRegex.exec(msg.text)) !== null) {
-        const shortURL = match[0];
-        fullURLRegex.lastIndex = 0; // Otherwise sometimes getASINFromFullUrl won't succeed
-        const url = await getLongUrl(shortURL);
-
-        if (url != null) {
-          if (raw_links) {
-            replacements.push({
-              asin: null,
-              expanded_url: url.fullURL,
-              fullURL: shortURL,
-            });
-          } else {
-            replacements.push({
-              asin: getASINFromFullUrl(url.fullURL),
-              fullURL: shortURL,
-            });
-          }
-        }
-      }
-
-      if (replacements.length > 0) {
-        const text = await buildMessage(
-          msg.chat,
-          msg.text,
-          replacements,
-          msg.from
-        );
-        const deleted = deleteAndSend(msg.chat, msg.message_id, text);
-
-        if (replacements.length > 1) {
-          replacements.forEach((element) => {
-            log(
-              "Long URL " +
-                element.fullURL +
-                " -> ASIN " +
-                element.asin +
-                " from " +
-                buildMention(msg.from) +
-                (deleted ? " (original message deleted)" : "")
-            );
-          });
-        } else {
-          log(
-            "Long URL " +
-              replacements[0].fullURL +
-              " -> ASIN " +
-              replacements[0].asin +
-              " from " +
-              buildMention(msg.from) +
-              (deleted ? " (original message deleted)" : "")
-          );
-        }
-      }
-    } else {
-      log(
-        `Ignored message from ${buildMention(
-          msg.from
-        )} because it is included in the IGNORE_USERS env variable`
-      );
-    }
-  } catch (e) {
+} catch (e) {
     log(
       "ERROR, please file a bug report at https://github.com/LucaTNT/telegram-bot-amazon"
-    );
-    console.log(e);
-  }
+}
 });
