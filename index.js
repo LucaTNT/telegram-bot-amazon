40f73cecf6d27bf0de09bd4c7b6dc961cbaecee2
Luca Zorzi <luca@lucazorzi.net>
2022-12-04 17:19:34 +0100

    First commit of custom implementation

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ License: MIT
 const TelegramBot = require("node-telegram-bot-api");
 const fetch = require("node-fetch");
 
-const fullURLRegex =
-  /https?:\/\/(([^\s]*)\.)?amazon\.([a-z.]{2,5})(\/d\/([^\s]*)|\/([^\s]*)\/?(?:dp|o|gp|-)\/)(aw\/d\/|product\/)?(B[0-9]{2}[0-9A-Z]{7}|[0-9]{9}(?:X|[0-9]))([^\s]*)/gi;
-const shortURLRegex = /https?:\/\/(([^\s]*)\.)?amzn\.to\/([0-9A-Za-z]+)/gi;
 const URLRegex =
   /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi;
 
@@ -24,29 +21,6 @@ if (!process.env.TELEGRAM_BOT_TOKEN) {
   console.log("Missing TELEGRAM_BOT_TOKEN env variable");
   process.exit(1);
 }
-
-if (!process.env.AMAZON_TAG) {
-  console.log("Missing AMAZON_TAG env variable");
-  process.exit(1);
-}
-
-const shorten_links =
-  process.env.SHORTEN_LINKS && process.env.SHORTEN_LINKS == "true";
-const bitly_token = process.env.BITLY_TOKEN;
-if (shorten_links && !bitly_token) {
-  console.log(
-    "Missing BITLY_TOKEN env variable (required when SHORTEN_LINKS is true)"
-  );
-  process.exit(1);
-}
-
-const raw_links = process.env.RAW_LINKS && process.env.RAW_LINKS == "true";
-const check_for_redirects =
-  process.env.CHECK_FOR_REDIRECTS && process.env.CHECK_FOR_REDIRECTS == "true";
-const check_for_redirect_chains =
-  process.env.CHECK_FOR_REDIRECT_CHAINS &&
-  process.env.CHECK_FOR_REDIRECT_CHAINS == "true";
-const max_redirect_chain_depth = process.env.MAX_REDIRECT_CHAIN_DEPTH || 2;
 
 var group_replacement_message;
 
@@ -60,21 +34,7 @@ if (!process.env.GROUP_REPLACEMENT_MESSAGE) {
   group_replacement_message = process.env.GROUP_REPLACEMENT_MESSAGE;
 }
 
-var amazon_tld;
-
-if (!process.env.AMAZON_TLD) {
-  console.log("Missing AMAZON_TLD env variable, using the default one (.com)");
-  amazon_tld = "com";
-} else {
-  amazon_tld = process.env.AMAZON_TLD;
-}
-
 const token = process.env.TELEGRAM_BOT_TOKEN;
-const amazon_tag = process.env.AMAZON_TAG;
-const rawUrlRegex = new RegExp(
-  `https?://(([^\\s]*)\\.)?amazon\\.${amazon_tld}/?([^\\s]*)`,
-  "ig"
-);
 
 var usernames_to_ignore = [];
 var user_ids_to_ignore = [];
@@ -103,65 +63,19 @@ function log(msg) {
   console.log(date + " " + msg);
 }
 
-async function shortenURL(url) {
-  const headers = {
-    Authorization: `Bearer ${bitly_token}`,
-    "Content-Type": "application/json",
-  };
-  const body = { long_url: url, domain: "bit.ly" };
-  try {
-    const res = await fetch("https://api-ssl.bitly.com/v4/shorten", {
-      method: "post",
-      headers: headers,
-      body: JSON.stringify(body),
-    });
-    const result = await res.json();
-    if (result.link) {
-      return result.link;
-    } else {
-      log("Error in bitly response " + JSON.stringify(result));
-      return url;
-    }
-  } catch (err) {
-    log(`Error in bitly response ${err}`);
-    return url;
-  }
-}
-
-function buildAmazonUrl(asin) {
-  return `https://www.amazon.${amazon_tld}/dp/${asin}?tag=${amazon_tag}`;
-}
-
-function buildRawAmazonUrl(element) {
-  const url = element.expanded_url ? element.expanded_url : element.fullURL;
-  const strucutredURL = new URL(url);
-  strucutredURL.searchParams.set("tag", amazon_tag);
-
-  return strucutredURL.toString();
-}
-
-async function getAmazonURL(element) {
-  const url =
-    element.asin != null
-      ? buildAmazonUrl(element.asin)
-      : buildRawAmazonUrl(element);
-  return shorten_links ? await shortenURL(url) : url;
-}
-
 function buildMention(user) {
   return user.username
     ? "@" + user.username
     : user.first_name + (user.last_name ? " " + user.last_name : "");
 }
 
-async function buildMessage(chat, message, replacements, user) {
+function buildMessage(chat, message, replacements, user) {
   if (isGroup(chat)) {
     var affiliate_message = message;
-    for await (const element of replacements) {
-      const sponsored_url = await getAmazonURL(element);
+    for (const element of replacements) {
       affiliate_message = affiliate_message.replace(
-        element.fullURL,
-        sponsored_url
+        element.find,
+        element.replace
       );
     }
 
@@ -172,13 +86,9 @@ async function buildMessage(chat, message, replacements, user) {
       .replace("{ORIGINAL_MESSAGE}", message);
   } else {
     var text = "";
-    if (replacements.length > 1) {
-      for await (const element of replacements) {
-        text += "• " + (await getAmazonURL(element)) + "\n";
+      for (const element of replacements) {
+        text += (replacements.length > 1 ? "• " : "") + (element.replace) + "\n";
       }
-    } else {
-      text = await getAmazonURL(replacements[0]);
-    }
 
     return text;
   }
@@ -220,43 +130,10 @@ function deleteAndSend(msg, text) {
   return deleted;
 }
 
-function getASINFromFullUrl(url) {
-  const match = fullURLRegex.exec(url);
-
-  return match != null ? match[8] : url;
-}
-
-async function getLongUrl(shortURL, chain_depth = 0) {
-  try {
-    chain_depth++;
-    let res = await fetch(shortURL, { redirect: "manual" });
-    let fullURL = res.headers.get("location");
-
-    if (check_for_redirect_chains && chain_depth < max_redirect_chain_depth) {
-      var nextRedirect = null;
-      if (fullURL !== null) {
-        nextRedirect = await getLongUrl(fullURL, chain_depth);
-      }
-
-      if (fullURL === null) {
-        return { fullURL: shortURL, shortURL: shortURL };
-      } else {
-        return { fullURL: nextRedirect["fullURL"], shortURL: shortURL };
-      }
-    } else {
-      if (fullURL === null) {
-        return { fullURL: shortURL, shortURL: shortURL };
-      } else {
-        return { fullURL: fullURL, shortURL: shortURL };
-      }
-    }
-  } catch (err) {
-    log("Short URL " + shortURL + " -> ERROR");
-    return null;
-  }
-}
 
 function replaceTextLinks(msg) {
+  var links = [];
+
   if (msg.entities) {
     var offset_shift = 0;
     msg.entities.forEach((entity) => {
@@ -273,13 +150,44 @@ function replaceTextLinks(msg) {
         new_text += entity.url;
         offset_shift = entity.url.length - length;
 
+        links.push(entity.url)
+
         new_text += msg.text.substring(offset + length);
 
         msg.text = new_text;
       }
+      if (entity.type == "url") {
+        links.push(msg.text.substring(entity.offset, entity.offset + entity.length))
+      }
     });
+  }
+    return {text: msg.text, links: links};
+}
 
-    return msg.text;
+async function getReplacements(links) {
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  const body = { links: links };
+
+  try {
+    const res = await fetch("https://stocazzo123.com/affiliate", {
+      method: "post",
+      headers: headers,
+      body: JSON.stringify(body),
+    });
+    const result = await res.json();
+
+    if (result["replacements"]) {
+      return result["replacements"];
+    } else {
+      log("Error in sponsor API response " + JSON.stringify(result));
+
+      return [];
+    }
+  } catch (err) {
+    log(`Error in sponsor API response ${err}`);
+    return [];
   }
 }
 
@@ -294,75 +202,28 @@ bot.on("message", async (msg) => {
         !user_ids_to_ignore.includes(from_id)) ||
       !isGroup(msg.chat)
     ) {
-      msg.text = replaceTextLinks(msg);
-
+      const text_and_links = replaceTextLinks(msg);
+      msg.text = text_and_links['text']
+      
       msg.text = msg.text || msg.caption;
       msg.captionSavedAsText = msg.text == msg.caption;
 
-      if (check_for_redirects) {
+      if (msg.captionSavedAsText) {
         URLRegex.lastIndex = 0;
-        var longURLReplacements = [];
         while ((match = URLRegex.exec(msg.text)) !== null) {
-          shortURLRegex.lastIndex = 0;
-          rawUrlRegex.lastIndex = 0;
-          if (
-            shortURLRegex.exec(match[0]) === null &&
-            rawUrlRegex.exec(match[0]) === null
-          ) {
-            log(`Found non-Amazon URL ${match[0]}`);
-            let longURL = await getLongUrl(match[0]);
-            longURLReplacements.push(longURL);
-          }
-        }
-
-        longURLReplacements.forEach((element) => {
-          if (element.fullURL !== null) {
-            msg.text = msg.text.replace(element.shortURL, element.fullURL);
-          }
-        });
-      }
-
-      shortURLRegex.lastIndex = 0;
-      var replacements = [];
-      var match;
-      if (raw_links) {
-        rawUrlRegex.lastIndex = 0;
-
-        while ((match = rawUrlRegex.exec(msg.text)) !== null) {
-          const fullURL = match[0];
-
-          replacements.push({ asin: null, fullURL: fullURL });
-        }
-      } else {
-        fullURLRegex.lastIndex = 0;
-
-        while ((match = fullURLRegex.exec(msg.text)) !== null) {
-          const asin = match[8];
-          const fullURL = match[0];
-          replacements.push({ asin: asin, fullURL: fullURL });
+          text_and_links['links'].push(match[0]);
         }
       }
 
-      while ((match = shortURLRegex.exec(msg.text)) !== null) {
-        const shortURL = match[0];
-        fullURLRegex.lastIndex = 0; // Otherwise sometimes getASINFromFullUrl won't succeed
-        const url = await getLongUrl(shortURL);
+      // var replacements = [];
+      // text_and_links['links'].forEach((link) => {
+      //   const replacement = {find: link, replace: link.toUpperCase()};
+      //   replacements.push(replacement);
+      // });
 
-        if (url != null) {
-          if (raw_links) {
-            replacements.push({
-              asin: null,
-              expanded_url: url.fullURL,
-              fullURL: shortURL,
-            });
-          } else {
-            replacements.push({
-              asin: getASINFromFullUrl(url.fullURL),
-              fullURL: shortURL,
-            });
-          }
-        }
-      }
+      const replacements = await getReplacements(text_and_links['links']);
+
+      console.log(replacements);
 
       if (replacements.length > 0) {
         const text = await buildMessage(
@@ -372,30 +233,17 @@ bot.on("message", async (msg) => {
           msg.from
         );
         const deleted = deleteAndSend(msg, text);
-
-        if (replacements.length > 1) {
-          replacements.forEach((element) => {
-            log(
-              "Long URL " +
-                element.fullURL +
-                " -> ASIN " +
-                element.asin +
-                " from " +
-                buildMention(msg.from) +
-                (deleted ? " (original message deleted)" : "")
-            );
-          });
-        } else {
+        replacements.forEach((element) => {
           log(
-            "Long URL " +
-              replacements[0].fullURL +
-              " -> ASIN " +
-              replacements[0].asin +
+            "Replaced URL " +
+              element.find +
+              " with URL " +
+              element.replace +
               " from " +
               buildMention(msg.from) +
               (deleted ? " (original message deleted)" : "")
           );
-        }
+        });
       }
     } else {
       log(
